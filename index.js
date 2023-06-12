458d9f4b660a26bd8590a65682faf5b78e80bced
enzofcna <enzolbd@gmail.com>
2023-06-12 16:12:49 +0200

    fix : incorrect url

--- index.js
+++ index.js
@@ -10,35 +10,40 @@ exports.run = async ({ pluginConfig, processingConfig, tmpDir, axios, log, patch
   const formData = new FormData()
 
   await download(processingConfig, tmpDir, axios, log)
-  await process(tmpDir, log)
+  const size = fs.statSync(path.join(tmpDir, 'finess.csv')).size
+  if (size !== 0) {
+    await process(tmpDir, log)
 
-  if (processingConfig.datasetMode === 'update') {
-    await log.step('Mise à jour du jeu de données')
-    await log.info('Mise à jour du schéma')
-    formData.append('schema', JSON.stringify(schema))
+    if (processingConfig.datasetMode === 'update') {
+      await log.step('Mise à jour du jeu de données')
+      await log.info('Mise à jour du schéma')
+      formData.append('schema', JSON.stringify(schema))
+    } else {
+      formData.append('schema', JSON.stringify(schema))
+      formData.append('title', processingConfig.dataset.title)
+      await log.step('Création du jeu de données')
+    }
+    const filePath = path.join(tmpDir, 'etablissements_geolocalises.csv')
+    formData.append('file', await fs.createReadStream(filePath), { filename: path.parse(filePath).base })
+
+    formData.getLength = util.promisify(formData.getLength)
+    const contentLength = await formData.getLength()
+    const dataset = (await axios({
+      method: 'post',
+      url: (processingConfig.dataset && processingConfig.dataset.id) ? `api/v1/datasets/${processingConfig.dataset.id}` : 'api/v1/datasets',
+      data: formData,
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
+      headers: { ...formData.getHeaders(), contentLength }
+    })).data
+
+    if (processingConfig.datasetMode === 'update') {
+      await log.info(`jeu de donnée mis à jour, id="${dataset.id}", title="${dataset.title}"`)
+    } else {
+      await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
+      await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
+    }
   } else {
-    formData.append('schema', JSON.stringify(schema))
-    formData.append('title', processingConfig.dataset.title)
-    await log.step('Création du jeu de données')
-  }
-  const filePath = path.join(tmpDir, 'etablissements_geolocalises.csv')
-  formData.append('file', await fs.createReadStream(filePath), { filename: path.parse(filePath).base })
-
-  formData.getLength = util.promisify(formData.getLength)
-  const contentLength = await formData.getLength()
-  const dataset = (await axios({
-    method: 'post',
-    url: (processingConfig.dataset && processingConfig.dataset.id) ? `api/v1/datasets/${processingConfig.dataset.id}` : 'api/v1/datasets',
-    data: formData,
-    maxContentLength: Infinity,
-    maxBodyLength: Infinity,
-    headers: { ...formData.getHeaders(), contentLength }
-  })).data
-
-  if (processingConfig.datasetMode === 'update') {
-    await log.info(`jeu de donnée mis à jour, id="${dataset.id}", title="${dataset.title}"`)
-  } else {
-    await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
-    await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
+    await log.error('Fichier vide, l\'URL a peut-être changé ou a été mal renseigné')
   }
 }
